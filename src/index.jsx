291416546ad642259d3acb4eb57fddd06de2cde4
Larsmara <lars.m.antonsen@gmail.com>
2021-05-27 23:09:02 +0200

    Working again

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import { FirebaseContext } from '@context';
 import { FieldValue, firebase } from './lib/firebase';
@@ -11,7 +12,9 @@ ReactDOM.render(
   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
     <ChakraProvider theme={theme}>
       <CSSReset />
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </FirebaseContext.Provider>,
   document.getElementById('root')
