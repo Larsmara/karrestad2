879fa2357c91cc84ab3e03d4b4c270a638216c20
Larsmara <lars.m.antonsen@gmail.com>
2021-06-08 20:55:35 +0200

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyAt0oYn4nDCM4uaBah-q5asQVlLOtejcpE',
-  authDomain: 'karrestad2-dev.firebaseapp.com',
-  projectId: 'karrestad2-dev',
-  storageBucket: 'karrestad2-dev.appspot.com',
-  messagingSenderId: '596289995554',
-  appId: '1:596289995554:web:4a4c51fd71f40e81ca9b7b'
+  apiKey: 'AIzaSyDL3kY8RmBtQUk5K4K42hBfiCr6oddaIsw',
+  authDomain: 'karrestad-2.firebaseapp.com',
+  projectId: 'karrestad-2',
+  storageBucket: 'karrestad-2.appspot.com',
+  messagingSenderId: '783667711080',
+  appId: '1:783667711080:web:c6016f0ef365b3ac373fdd',
+  measurementId: 'G-NTYRZ1BC72'
 };
 
 const firebase = Firebase.initializeApp(config);
